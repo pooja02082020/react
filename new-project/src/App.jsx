1416265a1d643aa0cf4387e6748ee6050420e78b
pooja02082020 <lohanipooja77@gmail.com>
2025-11-06 21:40:36 -0500

    useRef

--- new-project/src/App.jsx
+++ new-project/src/App.jsx
@@ -1,48 +1,30 @@
-import React, { Component } from 'react'
-//import './App.css'
-import InnerComp from './components/InnerComp'
-import styles from './App.module.css'
-import styled from 'styled-components'
+import React, { useRef } from 'react'
 
-export class App extends Component {
-  render() {
-    // inline style
-    // const hstyle ={
-    // color:"brown",
-    // backgroundColor:"blue"
-    // }
-    // 
+function App() {
+  const myRefInput = useRef(null);
 
-    //Create  a tag
-    let Title = styled.h1
-      `color: green;
-    font-size:40px;
-    background-color:yellow`
-
-    let Btn = styled.button
-      `
-      color:green;
-      background-color:yellow;
-      `
-    return (
-      <div>
-      <Btn>Click Me!</Btn>
-        {/* <h1 style={hstyle}>Welcome to my Webpage</h1> */}
-        <h1 className={styles.heading}>Welcome to my Webpage</h1>
-        <hr />
-        <Title>This is an example of styled element</Title>
-        <h1 >another h1 tag</h1>
-        <h4 id='heading'> List of Skills</h4>
-        <ul className='list'>
-          <li>HTML</li>
-          <li>JavaScript</li>
-          <li>Java</li>
-          <li>Python</li>
-        </ul>
-        <InnerComp />
-      </div>
-    )
+  let check = () =>{
+    console.log(myRefInput)
+    myRefInput.current.focus()
+    myRefInput.current.style.color = "blue"
+     myRefInput.current.placeholder ="write smth "
+      myRefInput.current.value = 4356789
   }
+
+  const toggleIp =()=>{
+    if(myRefInput.current.style.display !="none")
+      myRefInput.current.style.display ="none"
+    else{
+      myRefInput.current.style.display = "inline"
+    }
+  }
+  return (
+    <div>
+      <button onClick={toggleIp}>Toggle</button>
+      <input ref={myRefInput} type="text"  placeholder='Enter Username'/>
+      <button onClick={check}>Focus</button>
+    </div>
+  )
 }
 
 export default App
