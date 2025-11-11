03e2747fa6ee1bda4f03088513c91c4fd1cc4d0d
pooja02082020 <lohanipooja77@gmail.com>
2025-11-10 20:30:45 -0500

    lazy loading

--- new-project/src/App.jsx
+++ new-project/src/App.jsx
@@ -1,41 +1,17 @@
-import React, { useReducer } from 'react'
-// INitial object set to 0
-const inData ={
-  name:'',
-  password:'',
-  email:'',
-  city:'',
-  country:''
-}
-
-let reducer =(state,action)=>{
-  // console.log("State: ",state, "Action: ",action)
-  // console.log("Action: ",action)
-
-  return{...state,[action.type]:action.val}
-
-}
-
+import React, { lazy, Suspense, useState } from 'react'
+// import Visitor from './components/Visitor'
+let Comp = lazy(()=>import('./components/Visitor'))
 
 function App() {
-  let[state,dispatch]=useReducer(reducer,inData)
-  console.log(state)
+  let[load,setLoad]=useState(false)
   return (
     <div>
-      <input type="text" onChange={(event)=>dispatch({val:event.target.value,type:'name'})} placeholder='Enter Name' /><br /><br />
-      <input type="text" onChange={(event)=>dispatch({val:event.target.value,type:'password'})} placeholder='Enter Password' /><br /><br />
-      <input type="text" onChange={(event)=>dispatch({val:event.target.value,type:'email'})} placeholder='Enter Email' /><br /><br />
-      <input type="text" onChange={(event)=>dispatch({val:event.target.value,type:'city'})} placeholder='Enter City' /><br /><br />
-      <input type="text" onChange={(event)=>dispatch({val:event.target.value,type:'country'})} placeholder='Enter Country' /><br /><br />
-    
-    <br /><br />
-    <ul>
-      <li>Name: {state.name}</li>
-      <li>Password:  {state.password}</li>
-      <li>Email: {state.email} </li>
-      <li>City:{state.city} </li>
-      <li>Country:{state.country} </li>
-    </ul>
+      {load?<Suspense fallback ={<h2>Loading....</h2>}><Comp/></Suspense>:null
+      }
+      {/* {
+        // load? <Visitor/>:null
+      // } */}
+     <button onClick={()=>setLoad(true)}>Load Visitor</button>
     </div>
   )
 }
---
+++ new-project/src/components/Visitor.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+function Visitor() {
+  return (
+    <div>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+      <h1>Visitor numbers</h1>
+    </div>
+  )
+}
+
+export default Visitor
